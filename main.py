c8fa0fa3dcc33f8ff2b8082b1cf0f41dc7b0eab5
Elizabeth Yarrow <eyarrow@outlook.com>
2023-04-19 12:16:41 -0700

    Experience being generated successfully with sample data

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ import experience
 
 if __name__ == '__main__':
   summary_section = summary.generate_summary('summary.txt')
-  experience_section = experience.add_experience_bullets() 
+
+  # Gather skills
+  adobe_file = open("Adobe_skills.txt", "r")
+  adobe_skills = adobe_file.read()
+  adobe_list = adobe_skills.split("\n")
+  
+  pcc_file = open("PCC_skills.txt", "r")
+  pcc_skills = pcc_file.read()
+  pcc_list = pcc_skills.split("\n")
+
+
+  experience_section = experience.add_experience_bullets(adobe_list, pcc_list) 
 
   # Write summary section to a file
   file = open("summary.tex", "w")
