219cab820d76e5255ec191a510b7c6193ca6b133
Elizabeth Yarrow <eyarrow@outlook.com>
2023-04-20 10:36:57 -0700

    Add path information so files are written to the correct directory

--- experience.py
+++ experience.py
@@ -2,7 +2,7 @@ cv_entry_header = "%---------------------------------------------------------" +
 cv_entry_footer = "      \end{cvitems}" + "\n" + "    }" + "\n\n" "%---------------------------------------------------------" "\n"
 cv_job1 = r"    {Technical Support Engineer II} % Job title" + "\n" + r"    {Adobe} % Organization" + "\n" + r"    {Portland, OR} % Location" + "\n" + "    {Jun. 2019 - Sep. 2022} % Date(s)" + "\n" + "    {" + "\n" + r"      \begin{cvitems} % Description(s) of tasks/responsibilities" + "\n"
 cv_job2 = r"    {Systems Technician} % Job title" + "\n" + r"    {Portland Community College} % Organization" + "\n" + r"    {Portland, OR} % Location" + "\n" + "    {Mar. 2015 - Jun. 2019} % Date(s)" + "\n" + "    {" + "\n" + r"      \begin{cvitems} % Description(s) of tasks/responsibilities" + "\n"
-
+cventries_footer = "\end{cventries}"
 def return_formatted_skill(skill: str) -> str:
     return "        \item" + " " + r"{" + skill + "}" + "\n"
 
@@ -16,6 +16,6 @@ def return_formatted_bullets(skill_list: list) -> str:
 def add_experience_bullets(job1: list, job2: list) -> str:
     experience_bullets_job1 = return_formatted_bullets(job1)
     experience_bullets_job2 = return_formatted_bullets(job2)
-    output = cv_entry_header + cv_job1 + experience_bullets_job1 + cv_entry_footer + cv_job2 + experience_bullets_job2 + cv_entry_footer
+    output = cv_entry_header + cv_job1 + experience_bullets_job1 + cv_entry_footer + cv_entry_header + cv_job2 +  experience_bullets_job2 + cv_entry_footer + cventries_footer
     return output
     
--- main.py
+++ main.py
@@ -1,28 +1,63 @@
-import skill
 import summary
 import experience
+import tkinter as tk
+
+fields = ('File Path')
+
+
+  # # Gather skills
+  # adobe_file = open("Adobe_skills.txt", "r")
+  # adobe_skills = adobe_file.read()
+  # adobe_list = adobe_skills.split("\n")
+  
+  # pcc_file = open("PCC_skills.txt", "r")
+  # pcc_skills = pcc_file.read()
+  # pcc_list = pcc_skills.split("\n")
+
+
+  # experience_section = experience.add_experience_bullets(adobe_list, pcc_list) 
+
+  # # Write summary section to a file
+  # file = open("summary.tex", "w")
+  # file.write(summary_section)
+  # file.close
+
+  # #Write experience bullet points to file
+  # f = open("experience.tex", "a")
+  # f.write(experience_section)
+  # f.close
+def build_resume(event) -> str:
+    path = E1.get()
+
+    # Gather skill files
+    adobe_file = open(f"{path}/Adobe_skills.txt", "r")
+    adobe_skills = adobe_file.read()
+    adobe_list = adobe_skills.split("\n")
+
+    pcc_file = open(f"{path}/PCC_skills.txt", "r")
+    pcc_skills = pcc_file.read()
+    pcc_list = pcc_skills.split("\n")
+
+    experience_section = experience.add_experience_bullets(adobe_list, pcc_list)
+    summary_section = summary.generate_summary(f'{path}/summary.txt')
+    # Write summary section to a file
+    file = open(f"{path}/resume/summary.tex", "w")
+    file.write(summary_section)
+    file.close 
+
+     #Write experience bullet points to file
+    f = open(f"{path}/resume/experience.tex", "a")
+    f.write(experience_section)
+    f.close
+
 
 if __name__ == '__main__':
-  summary_section = summary.generate_summary('summary.txt')
+    root = tk.Tk()
+    root.geometry("400x100")
+    L1 = tk.Label(root, text="File Path").pack()
+    E1 = tk.Entry(root)
+    E1.bind("<Return>", build_resume)
+    E1.pack()
+    root.mainloop()
 
-  # Gather skills
-  adobe_file = open("Adobe_skills.txt", "r")
-  adobe_skills = adobe_file.read()
-  adobe_list = adobe_skills.split("\n")
   
-  pcc_file = open("PCC_skills.txt", "r")
-  pcc_skills = pcc_file.read()
-  pcc_list = pcc_skills.split("\n")
-
-
-  experience_section = experience.add_experience_bullets(adobe_list, pcc_list) 
-
-  # Write summary section to a file
-  file = open("summary.tex", "w")
-  file.write(summary_section)
-  file.close
-
-  #Write experience bullet points to file
-  f = open("experience.tex", "a")
-  f.write(experience_section)
-  f.close
