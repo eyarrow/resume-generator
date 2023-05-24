e3fb85b69ff9eec136407f71966cb8b392c22aa9
Elizabeth Yarrow <eyarrow@outlook.com>
2023-05-24 09:43:36 -0700

    Fixing adobe date of employment to 2021

--- main.py
+++ main.py
@@ -4,28 +4,6 @@ import tkinter as tk
 
 fields = ('File Path')
 
-
-  # # Gather skills
-  # adobe_file = open("Adobe_skills.txt", "r")
-  # adobe_skills = adobe_file.read()
-  # adobe_list = adobe_skills.split("\n")
-  
-  # pcc_file = open("PCC_skills.txt", "r")
-  # pcc_skills = pcc_file.read()
-  # pcc_list = pcc_skills.split("\n")
-
-
-  # experience_section = experience.add_experience_bullets(adobe_list, pcc_list) 
-
-  # # Write summary section to a file
-  # file = open("summary.tex", "w")
-  # file.write(summary_section)
-  # file.close
-
-  # #Write experience bullet points to file
-  # f = open("experience.tex", "a")
-  # f.write(experience_section)
-  # f.close
 def build_resume(event) -> str:
     path = E1.get()
 
